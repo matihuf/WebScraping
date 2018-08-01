356195124a5c60e286e188409be8ca7fa1158a60
matihuf <matihuf@hotmail.com>
2018-08-01 18:49:35 +0200

    add html node extensions

---
+++ WebScraping/Extensions/HTMLNodeExtension.cs
@@ -0,0 +1,57 @@
+﻿using HtmlAgilityPack;
+using System;
+
+namespace WebScraping.Extensions
+{
+    public static class HTMLNodeExtension
+    {
+        public static string GetStringValue(this HtmlNode htmlNode, string xpath)
+        {
+            if(string.IsNullOrEmpty(xpath))
+            {
+                return string.Empty;
+            }
+
+            HtmlNode node = htmlNode.SelectSingleNode(xpath);
+            if (node == null)
+            {
+                return string.Empty;
+            }
+
+            return node.InnerText;
+        }
+
+        public static DateTime GetDateTimeValue(this HtmlNode htmlNode, string xpath)
+        {
+            if (string.IsNullOrEmpty(xpath))
+            {
+                return DateTime.MinValue;
+            }
+
+            HtmlNode node = htmlNode.SelectSingleNode(xpath);
+
+            DateTime dateTime;
+            if (node != null && DateTime.TryParse(node.InnerText, out dateTime))
+            {
+                return dateTime;
+            }
+            return DateTime.MinValue;
+        }
+
+        public static string GetExtAttributeValue(this HtmlNode htmlNode, string xpath, string attribute)
+        {
+            if (string.IsNullOrEmpty(xpath) || string.IsNullOrEmpty(attribute))
+            {
+                return string.Empty;
+            }
+
+            HtmlNode node = htmlNode.SelectSingleNode(xpath);
+            if (node == null)
+            {
+                return string.Empty;
+            }
+
+            return node.GetAttributeValue(attribute, string.Empty);
+        }
+    }
+}
