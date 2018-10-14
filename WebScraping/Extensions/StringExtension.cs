c5336179a2881ec1cd8cfa7f58bb284ece488262
matihuf <matihuf@hotmail.com>
2018-10-14 21:35:52 +0200

    Add string extension to remove html tags

--- WebScraping/Extensions/StringExtension.cs
+++ WebScraping/Extensions/StringExtension.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using HtmlAgilityPack;
+using System.Text;
 
 namespace WebScraping.Extensions
 {
@@ -7,6 +8,18 @@ namespace WebScraping.Extensions
         public static string RemoveDiacritics(this string s)
         {
             return Encoding.ASCII.GetString(Encoding.GetEncoding("Cyrillic").GetBytes(s));
+        }
+
+        public static string RemoveHtmlTags(this string s)
+        {
+            if(s == null)
+            {
+                return string.Empty;
+            }
+
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(s);
+            return htmlDoc.DocumentNode.InnerText;
         }
     }
 }
