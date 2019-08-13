f672c24aea986d2a82ad4b15820b74d18c56698b
matihuf <matihuf@hotmail.com>
2019-08-13 19:00:03 +0200

    Html table service

---
+++ WebScraping/Services/HTML/Table/HtmlTableService.cs
@@ -0,0 +1,28 @@
+﻿using HtmlAgilityPack;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebScraping.Services.HTML.Table
+{
+    public class HtmlTableService : IHtmlTableService
+    {
+        public IEnumerable<HtmlNode> GetTrNodes(HtmlDocument htmlDocument, string xpath)
+        {
+            if (htmlDocument != null && htmlDocument.DocumentNode != null)
+            {
+                return htmlDocument.DocumentNode.SelectNodes(xpath).Elements("tr");
+            }
+            return new List<HtmlNode>();
+        }
+
+        public HtmlNode GetThNode(HtmlNode trNode)
+        {
+            return trNode.ChildNodes.FirstOrDefault(x => x.Name.Equals("th"));
+        }
+
+        public HtmlNode GetTdNode(HtmlNode trNode)
+        {
+            return trNode.ChildNodes.FirstOrDefault(x => x.Name.Equals("td"));
+        }
+    }
+}
---
+++ WebScraping/Services/HTML/Table/IHtmlTableService.cs
@@ -0,0 +1,12 @@
+﻿using HtmlAgilityPack;
+using System.Collections.Generic;
+
+namespace WebScraping.Services.HTML.Table
+{
+    public interface IHtmlTableService
+    {
+        IEnumerable<HtmlNode> GetTrNodes(HtmlDocument htmlDocument, string xpath);
+        HtmlNode GetThNode(HtmlNode trNode);
+        HtmlNode GetTdNode(HtmlNode trNode);
+    }
+}
