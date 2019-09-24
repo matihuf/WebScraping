84be7c120f2ac15c07926dce7005b852ff4d17cb
matihuf <matihuf@hotmail.com>
2019-09-24 13:17:13 +0200

    Add extension enum method. Get enum value from description

--- WebScraping/Extensions/EnumExtension.cs
+++ WebScraping/Extensions/EnumExtension.cs
@@ -12,5 +12,28 @@ namespace WebScraping.Extensions
             var attributes = memInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
             return ((DescriptionAttribute)attributes[0]).Description;
         }
+
+        public static int GetIntEnumFromDescription(this string description, Type enumType)
+        {
+            return (int)GetEnumFromDescription(description, enumType);
+        }
+
+        public static byte GetByteEnumFromDescription(this string description, Type enumType)
+        {
+            return (byte)GetEnumFromDescription(description, enumType);
+        }
+
+        private static object GetEnumFromDescription(string description, Type enumType)
+        {
+            foreach (var field in enumType.GetFields())
+            {
+                DescriptionAttribute attribute = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) as DescriptionAttribute;
+                if (attribute != null && attribute.Description.Equals(description))
+                {
+                    return field.GetValue(null);
+                }
+            }
+            return 0;
+        }
     }
 }
